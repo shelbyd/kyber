e54734b3e9d8db96fb88a97d5cfd66bc528a814d
Shelby Doolittle <shelby@shelbyd.com>
2022-04-28 01:03:43 -0400

    create struct to transform sequence of keys into commands

---
+++ gui/src/keyboard_control.rs
@@ -0,0 +1,193 @@
+use eframe::egui::{Key, Modifiers};
+use std::collections::*;
+
+pub struct KeyboardControl<A> {
+    map: HashMap<String, A>,
+    history: String,
+    expected_text: Option<String>,
+}
+
+impl<A: Clone> KeyboardControl<A> {
+    pub fn new(map: HashMap<&str, A>) -> Self {
+        Self {
+            map: map.into_iter().map(|(k, v)| (k.to_string(), v)).collect(),
+            history: String::new(),
+            expected_text: None,
+        }
+    }
+
+    pub fn on_key(&mut self, key: Key, modifiers: Modifiers) {
+        debug_assert_eq!(self.expected_text, None);
+
+        let as_str = format!("{:?}", key);
+        if as_str.len() > 1 {
+            self.history += &format!("<{}>", as_str);
+        } else if modifiers.shift_only() {
+            self.expected_text = Some(as_str);
+        } else {
+            self.expected_text = Some(as_str.to_lowercase());
+        };
+    }
+
+    pub fn on_text(&mut self, text: &str) {
+        if let Some(t) = &self.expected_text {
+            debug_assert_eq!(t, text);
+        }
+        self.expected_text = None;
+
+        self.history += text;
+    }
+
+    pub fn take(&mut self) -> Option<A> {
+        if let Some((seq, a)) = self
+            .map
+            .iter()
+            .find(|(seq, _)| self.history.starts_with(*seq))
+        {
+            self.history = self.history.split_off(seq.len());
+            return Some(a.clone());
+        }
+
+        let could_be_in_progress = self.map.keys().any(|k| k.starts_with(&self.history));
+        if could_be_in_progress {
+            return None;
+        }
+
+        if self.map.is_empty() {
+            self.history.clear();
+            return None;
+        }
+
+        self.history.remove(0);
+        self.take()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn unrecognized_is_none() {
+        let mut control = KeyboardControl::<()>::new(HashMap::new());
+
+        control.on_text("o");
+
+        assert_eq!(control.take(), None);
+    }
+
+    #[test]
+    fn single_char() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "j" => "down",
+        });
+
+        control.on_text("j");
+
+        assert_eq!(control.take(), Some("down"));
+    }
+
+    #[test]
+    fn char_sequence() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "of" => "open_file",
+        });
+
+        control.on_text("o");
+        control.on_text("f");
+
+        assert_eq!(control.take(), Some("open_file"));
+    }
+
+    #[test]
+    fn sequence_of_actions() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "j" => "cursor_down",
+            "of" => "open_file",
+        });
+
+        control.on_text("j");
+        control.on_text("o");
+        control.on_text("f");
+
+        assert_eq!(control.take(), Some("cursor_down"));
+        assert_eq!(control.take(), Some("open_file"));
+        assert_eq!(control.take(), None);
+    }
+
+    #[test]
+    fn unrecognized_input() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "of" => "open_file",
+        });
+
+        control.on_text("j");
+        control.on_text("o");
+        control.on_text("f");
+
+        assert_eq!(control.take(), Some("open_file"));
+    }
+
+    #[test]
+    fn unrecognized_partial_input() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "of" => "open_file",
+        });
+
+        control.on_text("o");
+        control.on_text("r");
+        control.on_text("o");
+        control.on_text("f");
+
+        assert_eq!(control.take(), Some("open_file"));
+    }
+
+    #[test]
+    fn prefixed_free() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "f" => "cursor_down",
+            "of" => "open_file",
+        });
+
+        control.on_text("o");
+        control.on_text("f");
+
+        assert_eq!(control.take(), Some("open_file"));
+    }
+
+    #[test]
+    fn three_char_sequence() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "j" => "cursor_down",
+            "oof" => "open_file",
+        });
+
+        control.on_text("o");
+        control.on_text("o");
+        control.on_text("j");
+
+        assert_eq!(control.take(), Some("cursor_down"));
+    }
+
+    #[test]
+    fn captial_char() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "J" => "cursor_down",
+        });
+
+        control.on_text("J");
+
+        assert_eq!(control.take(), Some("cursor_down"));
+    }
+
+    #[test]
+    fn multi_letter_button() {
+        let mut control = KeyboardControl::new(maplit::hashmap! {
+            "<Tab>" => "cursor_down",
+        });
+
+        control.on_key(Key::Tab, Modifiers::default());
+
+        assert_eq!(control.take(), Some("cursor_down"));
+    }
+}
--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,18 +1,87 @@
-use eframe::{epi, egui};
+use eframe::{egui, epi};
+
+mod keyboard_control;
+use keyboard_control::*;
 
 fn main() {
     let app = App::default();
     eframe::run_native(Box::new(app), eframe::NativeOptions::default());
 }
 
-#[derive(Default)]
-struct App {}
+struct App {
+    keyboard_control: KeyboardControl<Action>,
+}
+
+impl Default for App {
+    fn default() -> Self {
+        Self {
+            keyboard_control: KeyboardControl::new(maplit::hashmap! {
+                "j" => Action::Cursor(Direction::Down),
+                "k" => Action::Cursor(Direction::Up),
+                "l" => Action::Cursor(Direction::Right),
+                "h" => Action::Cursor(Direction::Left),
+
+                "of" => Action::BeginOpenFile,
+            }),
+        }
+    }
+}
 
 impl epi::App for App {
     fn name(&self) -> &str {
         "Kyber"
     }
 
-    fn update(&mut self, _ctx: &egui::Context, _frame: &epi::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            for event in &ctx.input().events {
+                match event {
+                    egui::Event::Key {
+                        pressed,
+                        key,
+                        modifiers,
+                    } => {
+                        if *pressed {
+                            self.keyboard_control.on_key(*key, *modifiers);
+                        }
+                    }
+                    egui::Event::Text(s) => {
+                        self.keyboard_control.on_text(s);
+                    }
+                    _ => {}
+                }
+            }
+
+            while let Some(a) = self.keyboard_control.take() {
+                self.on_action(a);
+            }
+
+            ui.heading("Hello World!");
+        });
     }
+}
+
+impl App {
+    fn on_action(&mut self, action: Action) {
+        match action {
+            unhandled => {
+                unimplemented!("unhandled: {:?}", unhandled);
+            }
+        }
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Action {
+    BeginOpenFile,
+    Cursor(Direction),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
